966c3ca9c3b9550654d3104acd9955851c18973c
Wellington <wjas.ma@gmail.com>
2023-03-28 04:55:33 -0300

    Initial comit

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 28 Mar 2023 07:36:57 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGrey200": "#c1c1c4",
+  "ColorGrey500": "#4b4b4b",
+  "ColorGrey800": "#27272a",
+  "ColorGrey900": "#18181b",
+  "ColorPurple500": "#7c3aed"
+};
